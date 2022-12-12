0058c4464688dbe35f8628c7950431dc8a1b55e3
Shweta7896 <shweta@talkinglands.com>
2022-12-12 13:28:25 +0530

    hello msg commit

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
 
   return (
     <div className="App">
+      <h2>hello world</h2>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
